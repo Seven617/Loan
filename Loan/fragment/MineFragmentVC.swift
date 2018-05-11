3fcd9e21d395f7630448bcf844c80cdf95254529
Seven617 <740820890@qq.com>
2018-05-11 17:51:29 +0800

    first commit

---
+++ Loan/fragment/MineFragmentVC.swift
@@ -0,0 +1,35 @@
+//
+//  MineFragmentVC.swift
+//  Loan
+//
+//  Created by 冷少白 on 2018/5/11.
+//  Copyright © 2018年 kbfoo. All rights reserved.
+//
+
+import UIKit
+
+class MineFragmentVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

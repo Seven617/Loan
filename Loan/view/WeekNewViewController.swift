441186418250c35907b97ac45bb0bcdceee571ef
Seven617 <740820890@qq.com>
2018-06-15 13:27:14 +0800

    “6月15号更新”

---
+++ Loan/view/WeekNewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WeekNewViewController.swift
+//  Loan
+//
+//  Created by 冷少白 on 2018/6/15.
+//  Copyright © 2018年 kbfoo. All rights reserved.
+//
+
+import UIKit
+
+class WeekNewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
